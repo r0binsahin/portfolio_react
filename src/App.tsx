b44030b85b206ff39e50605192223f0572916ce4
r0binSahin <robin.sahin88@gmail.com>
2023-12-01 11:03:45 +0100

    (feat): context added, data uppdated

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,36 @@ import { IntroSection } from "./component/introSection/IntroSection";
 
 import { Navbar } from "./component/navbar/Navbar";
 import { DisplaySkills } from "./component/displaySkills/DisplaySkills";
+import { DataContext } from "./component/context/DataContext";
+import { getData } from "./services/dataService";
+import { IMyData } from "./models/IMyData";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [myData, setMyData] = useState<IMyData>({
+    skills: [],
+    projects: [],
+    otherProjects: [],
+  });
+
+  const setData = async () => {
+    const myData: IMyData = await getData();
+
+    setMyData(myData);
+  };
+
+  useEffect(() => {
+    setData();
+  }, [setData]);
+
   return (
     <>
-      <Navbar />
-      <IntroSection />
-      <DisplaySkills />
-
-      <Resume />
+      <DataContext.Provider value={myData}>
+        <Navbar />
+        <IntroSection />
+        <DisplaySkills />
+        <Resume />
+      </DataContext.Provider>
     </>
   );
 }
